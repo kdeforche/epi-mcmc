7ab0ef6047bbd865a18c37ccf98caf6706cace83
Koen Deforche <koen.deforche@blooloc.com>
2020-05-08 09:07:13 +0200

    estimate iterations needed

--- R/MCMC/fitMCMC.R
+++ R/MCMC/fitMCMC.R
@@ -30,13 +30,13 @@ colnames(batch) <- fit.paramnames
 ##
 it <- 0
 
-for (i in 1:10000) {
+## 27000 -> 10
+## 300000 -> 100 per iteratie -> minstens 3000 iteraties
+
+for (i in 1:3000) {
     out <<- MCMC.add.samples(out, n=1E4)
     batch <- data.frame(out$samples[seq(1, nrow(out$samples), 1E2), ])
     colnames(batch) <- fit.paramnames
     plot(ts(subset(batch, select=fitkeyparamnames)))
     write.csv(batch, file=outputfile)
-
-##    if (i == 1)
-##        break
 }
